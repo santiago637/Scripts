90f47353b98c460cda0dbdca9e5d4000dd9a35ca
santiago637 <santiaguireyes@gmail.com>
2025-12-29 19:09:01 -0400

    Replace GUILocalScript with HUBBotton in Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/santiago637/Scripts/main/MainLocalScript.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/santiago637/Scripts/main/GUILocalScript.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/santiago637/Scripts/main/HUBBotton.lua"))()
